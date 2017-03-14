d0c306a3ac86e9a204a76d96ecba31a3eff5524d
amyyiinfo <amyyi.info@gmail.com>
2017-03-13 23:22:18 -0400

    Add files via upload

---
+++ MultiplesOf3and5/MultiplesOf3and5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3and5
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            int number;
+
+            while (true)
+            {
+                Console.WriteLine("Enter the sum you would like to calculate:");
+                string strNumber = Console.ReadLine();
+                number = Convert.ToInt32(strNumber);
+
+                int multiplesOfThreeFive = 0;
+
+
+                for (int i = 1; i < number; i++)
+                {
+                    if ((i % 3 == 0) || (i % 5 == 0))
+                    {
+                        multiplesOfThreeFive = multiplesOfThreeFive + i;
+                    }
+
+                }
+
+
+
+                Console.WriteLine(multiplesOfThreeFive);
+
+                 
+            }
+
+            Console.Read();
+
+            }
+        }
+    }
+
